8fc31fcfac9859b1a11ca90667a9e81529888eeb
jarrenpatao <jarrenpatao@gmail.com>
2018-11-08 04:58:31 -0800

    added some variables, functions, and tried to fix js a little... but idkwtf I'm doing.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,38 @@
-var gueLef = 5;
+// Defined Variables
+var guessLeft = 5;
 var wins = 0;
 var losses = 0;
 var ties = 0;
-var computerChoices = ['a','e','i','o','u','h','k','l','m','n','p','w',"'"];
+var questionIndex = 0;
+var computerChoice = ['0','1','2','3','4','5','6','7','8','9'];
 
-document.write("Guess what letter I'm thinking of in the Hawaiian alphabet...");
+// Defined Main Functions
+function resetGame() {
+  var gueLef = 5;
+  var wins = 0;
+  var losses = 0;
+}
 
-document.onkeyup = function(event) {
-  var userGuess = event.key;
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  if (userGuess === computerChoices) {
-    wins++;
+// Main Process
+
+for(var i = 0; i < computerChoice(Math.floor(Math.random[i])); i++){
+  console.log(computerChoice[i]);
+}
+
+document.onkeyup = function(event){
+  if(event.key === "0" || event.key === "1" || event.key === "2" || event.key === "3" || event.key === "4" || event.key === "5" || event.key === "6" || event.key === "7" || event.key === "8" || event.key === "9"){
+    if(event.key === computerChoice){
+      alert("sweet!");
+      wins++;
+      } 
+      else {
+        alert("wrong.");
+        guessLeft--;
+      }
+    }
   }
-  document.getElementById(userInput);
+
+// Reset Game
+if (gueLef === 0){
+  resetGame();
 }
